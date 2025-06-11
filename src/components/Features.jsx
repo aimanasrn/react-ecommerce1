c231267491543fe8b58b1f16f59903fa50458e4e
Muhammad Aiman bin Kanasronsham <96905225+aimanasrn@users.noreply.github.com>
2025-06-11 23:07:54 +0800

    update

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Features = () => {
   return (
-    <section id='Features' className='h-[50vh]'>
-        features
+    <section id='Features' className='h-[50vh] px-4 py-20'>
+        <div>
+            aaa
+        </div>
     </section>
   )
 }
